09b1757b09223292dcd03f259626acda5666842c
Harish Krishna <harish.krsn@gmail.com>
2015-04-16 15:54:38 -0400

    .values for arrays

--- scripts/category.groovy
+++ scripts/category.groovy
@@ -1,7 +1,7 @@
 // expects category_weights passed in with weight mapping
 if (doc.containsKey('category')) {
   score = 0
-  categories=doc['category'].value;
+  categories=doc['category'].values;
   categories.each { category -> 
   	score = score + (( category in category_weights ) ? category_weights[ category ] : 0) 
   }
